c41b487e8cfb3caa80850dbbb82f114912755fa0
leeminji <minjis2s2s2@gmail.com>
2022-06-23 16:43:26 +0900

    테트리스 무빙아이템 트리 만드는중

--- js/tetris.js
+++ js/tetris.js
@@ -1,8 +1,42 @@
+//dom
 const play = document.querySelector(".play > ul");
 
-console.log(play)
+//setting
+const GAME_ROWS = 20;
+const GAME_COLS = 10;
+//variables
+let score = 0;
+let duration = 500;
+let downinternal;
+let tempMovingitem;
 
-for(let i=0; i<20; i++){
+const BLOCKS = {
+    tree: [
+        [[0,0],[0,1],[1,0],[1,1]],
+        [],
+        [],
+        [],
+    ]
+}
+
+const movingitem={
+    type : "",
+    direction : 0,
+    top: 0,
+    left: 0,
+};
+
+init()
+
+// functions
+function init(){
+    for(let i=0; i<GAME_ROWS; i++){
+        prependNewLine()
+    }
+}
+
+
+function prependNewLine(){
     const li = document.createElement("li");
     const ul = document.createElement("ul");
     for(let j=0; j<10; j++){
